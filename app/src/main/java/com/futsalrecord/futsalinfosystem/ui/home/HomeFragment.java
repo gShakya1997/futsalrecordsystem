564f544c1a76969a6f4e3c4c40497e6654e5328a
gunjan <gunjan.shakya3@gmail.com>
2019-12-02 18:17:46 +0545

    Font changed

--- app/src/main/java/com/futsalrecord/futsalinfosystem/ui/home/HomeFragment.java
+++ app/src/main/java/com/futsalrecord/futsalinfosystem/ui/home/HomeFragment.java
@@ -11,15 +11,30 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.futsalrecord.futsalinfosystem.R;
+import com.futsalrecord.futsalinfosystem.adapter.CustomersAdapter;
+import com.futsalrecord.futsalinfosystem.model.Customers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+    private RecyclerView customerRecyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        customerRecyclerView = root.findViewById(R.id.customerRecyclerView);
+        List<Customers> customersList = new ArrayList<>();
+        customersList.add(new Customers("Gunjan Shakya","gunjan.shakya3@gmail.com","9860915753","Male","Kathmandu",R.drawable.noimage));
+        customersList.add(new Customers("Yunish Shakya","gunjan.shakya3@gmail.com","9860915753","Male","Patan",R.drawable.noimage));
+        CustomersAdapter customersAdapter = new CustomersAdapter(getActivity(),customersList);
+        customerRecyclerView.setAdapter(customersAdapter);
+        customerRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return root;
     }
 }
