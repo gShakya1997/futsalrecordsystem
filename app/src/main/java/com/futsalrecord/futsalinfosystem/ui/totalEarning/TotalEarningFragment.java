366023b40356c4bd994a423ffde2bca6253f3085
gunjan <gunjan.shakya3@gmail.com>
2019-12-03 11:22:13 +0545

    Icon added and change in font and colors. Picasso is implemented

--- app/src/main/java/com/futsalrecord/futsalinfosystem/ui/totalEarning/TotalEarningFragment.java
+++ app/src/main/java/com/futsalrecord/futsalinfosystem/ui/totalEarning/TotalEarningFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.futsalrecord.futsalinfosystem.R;
+import com.squareup.picasso.Picasso;
 
 public class TotalEarningFragment extends Fragment {
+    private ImageView imageView;
+    private String imageUrl = "https://static.scientificamerican.com/blogs/cache/file/BB6F1FE0-4FDE-4E6E-A986664CE30602E4_source.jpg?w=590&h=800&2F8476C1-DF14-49BA-84FFE94218CC4933";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_total_earning, container, false);
+        imageView = root.findViewById(R.id.ivImage);
+        Picasso.get().load(imageUrl).into(imageView);
         return root;
     }
 }
