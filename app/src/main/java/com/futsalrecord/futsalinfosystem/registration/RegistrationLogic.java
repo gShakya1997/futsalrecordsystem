32fbaabe8b5f87be3eab687501e4441ee4a2d54b
gunjan <gunjan.shakya3@gmail.com>
2020-01-26 11:56:22 +0545

    Issue on hashing

--- app/src/main/java/com/futsalrecord/futsalinfosystem/registration/RegistrationLogic.java
+++ app/src/main/java/com/futsalrecord/futsalinfosystem/registration/RegistrationLogic.java
@@ -9,7 +9,7 @@ public class RegistrationLogic {
                     //"(?=.*[a-z])" +         //at least 1 lower case letter
                     //"(?=.*[A-Z])" +         //at least 1 upper case letter
                     "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=<>])" +    //at least 1 special character
+                    "(?=.*[@#$%^&+=<>_!*().,])" +    //at least 1 special character
                     ".{6,}" +               //at least 6 characters
                     "$");
 }
--- app/src/main/java/com/futsalrecord/futsalinfosystem/registration/UserRegistration.java
+++ app/src/main/java/com/futsalrecord/futsalinfosystem/registration/UserRegistration.java
@@ -25,6 +25,7 @@ import com.futsalrecord.futsalinfosystem.serverResponse.ImageResponse;
 import com.futsalrecord.futsalinfosystem.serverResponse.RegisterResponse;
 import com.futsalrecord.futsalinfosystem.strictMode.StrictModeClass;
 import com.futsalrecord.futsalinfosystem.url.Url;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.File;
@@ -41,12 +42,9 @@ import retrofit2.Response;
 public class UserRegistration extends AppCompatActivity {
     private TextInputLayout userRegUsername, userRegAddress, userRegEmail, userRegPhone,
             userRegPassword, userRegCPassword;
-    private Button btnUserRegister;
-    private ImageButton imgBtnUpload;
+    private Button btnUserNext;
     private RadioGroup rgRegGender;
     private RadioButton rbRegMale, rbRegFemale, rbRegOthers;
-    String imgPath;
-    private String imgName = "";
     private String gender;
 
     @Override
@@ -58,109 +56,35 @@ public class UserRegistration extends AppCompatActivity {
     }
 
     private void actionButtons() {
-        imgBtnUpload.setOnClickListener(new View.OnClickListener() {
+
+        btnUserNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                browseImg();
+                if (!validateUsername() | !validateEmail() | !validatePassword() |
+                        !validatePhone() | !validateConfirmPassword() | !validateAddress()) {
+                    return;
+                }
+                int selectGender = rgRegGender.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(selectGender);
+                gender = radioButton.getText().toString().trim();
+                Intent intent = new Intent(getBaseContext(), UserRegistration.class);
+                String username = userRegUsername.getEditText().getText().toString().trim();
+                String address = userRegAddress.getEditText().getText().toString().trim();
+                String email = userRegEmail.getEditText().getText().toString().trim();
+                String phone = userRegPhone.getEditText().getText().toString().trim();
+                String password = userRegPassword.getEditText().getText().toString().trim();
+                intent.putExtra("username", username);
+                intent.putExtra("address", address);
+                intent.putExtra("email", email);
+                intent.putExtra("phone", phone);
+                intent.putExtra("password", password);
+                intent.putExtra("gender", gender);
+                startActivity(intent);
+                Intent intentActivity = new Intent(getApplicationContext(), UserProfilePic.class);
+                startActivity(intentActivity);
             }
         });
-
-        btnUserRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register();
-                saveImgOnly();
-                if (!register()) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
     }
-
-    private boolean register() {
-        if (validateUsername() || validateEmail() || validatePassword() ||
-                validatePhone() || validateConfirmPassword() || validateAddress() ||
-                validateGender()) {
-            return false;
-        } else {
-            String username = userRegUsername.getEditText().getText().toString().trim();
-            String address = userRegAddress.getEditText().getText().toString().trim();
-            String email = userRegEmail.getEditText().getText().toString().trim();
-            String phone = userRegPhone.getEditText().getText().toString().trim();
-            String password = userRegPassword.getEditText().getText().toString().trim();
-
-            Users users = new Users(username, address, email, phone, password, gender, imgName);
-            UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
-            Call<RegisterResponse> registerResponseCall = usersAPI.registerUser(users);
-
-            registerResponseCall.enqueue(new Callback<RegisterResponse>() {
-                @Override
-                public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
-                    if (!response.isSuccessful()) {
-                        Toast.makeText(UserRegistration.this, "Code " + response.code(), Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    Toast.makeText(UserRegistration.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onFailure(Call<RegisterResponse> call, Throwable t) {
-                    Toast.makeText(UserRegistration.this, "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (data == null) {
-                Toast.makeText(this, "Please select and image", Toast.LENGTH_SHORT).show();
-            }
-        }
-        Uri uri = data.getData();
-        imgBtnUpload.setImageURI(uri);
-        imgPath = getRealPathFromUri(uri);
-    }
-
-    private String getRealPathFromUri(Uri uri) {
-        String[] projection = {MediaStore.Images.Media.DATA};
-        CursorLoader cursorLoader = new CursorLoader(getApplicationContext(), uri, projection,
-                null, null, null);
-        Cursor cursor = cursorLoader.loadInBackground();
-        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String result = cursor.getString(colIndex);
-        cursor.close();
-        return result;
-    }
-
-    private void browseImg() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, 0);
-    }
-
-    private void saveImgOnly() {
-        File file = new File(imgPath);
-        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-        MultipartBody.Part body = MultipartBody.Part.createFormData("imageFile", file.getName(), requestBody);
-        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
-        Call<ImageResponse> responseCall = usersAPI.uploadImage(body);
-        StrictModeClass.StrictMode();
-        try {
-            Response<ImageResponse> imageResponseResponse = responseCall.execute();
-            imgName = imageResponseResponse.body().getFilename();
-            Toast.makeText(this, "Image inserted", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Toast.makeText(this, "Error " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-    }
-
 
     //Validation
     private boolean validateEmail() {
@@ -197,7 +121,7 @@ public class UserRegistration extends AppCompatActivity {
         if (regPhone.isEmpty()) {
             userRegPhone.setError("Required");
             return false;
-        } else if (regPhone.length() < 10 | regPhone.length() > 14) {
+        } else if (regPhone.length() <= 10) {
             userRegPhone.setError("Please enter a valid phone number");
             return false;
         } else {
@@ -245,18 +169,6 @@ public class UserRegistration extends AppCompatActivity {
         }
     }
 
-    private boolean validateGender() {
-        try {
-            int selectGender = rgRegGender.getCheckedRadioButtonId();
-            RadioButton radioButton = findViewById(selectGender);
-            gender = radioButton.getText().toString().trim();
-            return true;
-        } catch (Exception e) {
-            Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    }
-
     private void binding() {
         userRegUsername = findViewById(R.id.userRegUsername);
         userRegAddress = findViewById(R.id.userRegAddress);
@@ -264,8 +176,7 @@ public class UserRegistration extends AppCompatActivity {
         userRegPhone = findViewById(R.id.userRegPhone);
         userRegPassword = findViewById(R.id.userRegPassword);
         userRegCPassword = findViewById(R.id.userRegCPassword);
-        btnUserRegister = findViewById(R.id.btnUserRegister);
-        imgBtnUpload = findViewById(R.id.imgBtnUpload);
+        btnUserNext = findViewById(R.id.btnUserNext);
         rgRegGender = findViewById(R.id.rgRegGender);
         rbRegMale = findViewById(R.id.rbRegMale);
         rbRegFemale = findViewById(R.id.rbRegFemale);
