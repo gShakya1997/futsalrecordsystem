14cefda331df2b2db95394e7fb1c9e53b228eb29
gunjan <gunjan.shakya3@gmail.com>
2020-01-27 19:13:09 +0545

    login successfully

--- app/src/main/java/com/futsalrecord/futsalinfosystem/bll/LoginBLL.java
+++ app/src/main/java/com/futsalrecord/futsalinfosystem/bll/LoginBLL.java
@@ -1,6 +1,8 @@
 package com.futsalrecord.futsalinfosystem.bll;
 
+import com.futsalrecord.futsalinfosystem.api.FutsalAPI;
 import com.futsalrecord.futsalinfosystem.api.UsersAPI;
+import com.futsalrecord.futsalinfosystem.model.Futsal;
 import com.futsalrecord.futsalinfosystem.serverResponse.RegisterResponse;
 import com.futsalrecord.futsalinfosystem.url.Url;
 
@@ -23,6 +25,22 @@ public class LoginBLL {
                 isSuccess = true;
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
+
+    public boolean checkFutsal(String futsalName, String futsalPassword) {
+        FutsalAPI futsalAPI = Url.getInstance().create(FutsalAPI.class);
+        Call<RegisterResponse> futsalCall = futsalAPI.checkUser(futsalName, futsalPassword);
+        try {
+            Response<RegisterResponse> loginResponse = futsalCall.execute();
+            if (loginResponse.isSuccessful() && loginResponse.body().getStatus()
+                    .equals("Login successful")) {
+                Url.token += loginResponse.body().getToken();
+                isSuccess = true;
+            }
+        } catch (IOException e){
             e.printStackTrace();
         }
         return isSuccess;
